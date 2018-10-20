ba6fc148bc522e66ec91ca3b1ad897fea6c87d3f
fdmt55 <git@dinolab.ges.thm.de>
2018-10-20 07:49:14 +0200

    App has updated according to prism

--- StockTraderRI/App.xaml.cs
+++ StockTraderRI/App.xaml.cs
@@ -1,15 +1,32 @@
-﻿using System.Windows;
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System.Windows;
 
 namespace StockTraderRI
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
         {
-            base.OnStartup(e);
-            StockTraderRIBootstrapper bootstrapper = new StockTraderRIBootstrapper();
-            bootstrapper.Run();
-            this.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            base.ConfigureModuleCatalog(moduleCatalog);
+
+            moduleCatalog.AddModule(typeof(StockTraderRI.Modules.Market.MarketModule));
+            moduleCatalog.AddModule(typeof(StockTraderRI.Modules.Position.PositionModule));
+            moduleCatalog.AddModule(typeof(StockTraderRI.Modules.Watch.WatchModule));
+            moduleCatalog.AddModule(typeof(StockTraderRI.Modules.News.NewsModule));
+        }
+
+        protected override Window CreateShell()
+        {
+            // Use the container to create an instance of the shell.
+            Shell view = this.Container.Resolve<Shell>();
+            view.DataContext = this.Container.Resolve<ShellViewModel>();
+            return view;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
         }
     }
 }
