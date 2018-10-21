1fd9b7f8640ace36b5ae1039b03f495469aff17c
fdmt55 <git@dinolab.ges.thm.de>
2018-10-21 15:37:23 +0200

    Updated All Projects to .NET Framework 4.7.2 --> TargetFramework=.net472

--- StockTraderRI.Modules.Market/Properties/Resources.Designer.cs
+++ StockTraderRI.Modules.Market/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace StockTraderRI.Modules.Market.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -62,12 +62,12 @@ namespace StockTraderRI.Modules.Market.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
-        ///&lt;MarketItems xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; RefreshRate=&quot;5&quot;&gt;
+        ///&lt;MarketItems xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; RefreshRate=&quot;20&quot;&gt;
         ///  &lt;MarketItem TickerSymbol=&quot;STOCK0&quot; LastPrice=&quot;37.00&quot; Volume=&quot;1000&quot;&gt;&lt;/MarketItem&gt;
         ///  &lt;MarketItem TickerSymbol=&quot;STOCK1&quot; LastPrice=&quot;78.13&quot; Volume=&quot;100&quot;&gt;&lt;/MarketItem&gt;
         ///  &lt;MarketItem TickerSymbol=&quot;STOCK2&quot; LastPrice=&quot;23.80&quot; Volume=&quot;200&quot;&gt;&lt;/MarketItem&gt;
         ///  &lt;MarketItem TickerSymbol=&quot;STOCK3&quot; LastPrice=&quot;34.22&quot; Volume=&quot;300&quot;&gt;&lt;/MarketItem&gt;
-        ///  &lt;MarketItem Ticke [rest of string was truncated]&quot;;.
+        ///  &lt;MarketIte [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Market {
             get {
@@ -90,8 +90,7 @@ namespace StockTraderRI.Modules.Market.Properties {
         ///  &lt;MarketHistoryItem TickerSymbol=&quot;STOCK0&quot; Date=&quot;2008-02-23&quot;&gt;30.00&lt;/MarketHistoryItem&gt;
         ///  &lt;MarketHistoryItem TickerSymbol=&quot;STOCK0&quot; Date=&quot;2008-02-24&quot;&gt;35.00&lt;/MarketHistoryItem&gt;
         ///  &lt;MarketHistoryItem TickerSymbol=&quot;STOCK0&quot; Date=&quot;2008-02-25&quot;&gt;50.00&lt;/MarketHistoryItem&gt;
-        ///  &lt;MarketHistoryItem TickerSymbol=&quot;STOCK0&quot; Date=&quot;2008-02-26&quot;&gt;75.00&lt;/MarketHistoryItem&gt;
-        ///  &lt;Ma [rest of string was truncated]&quot;;.
+        ///  &lt;MarketHistoryItem TickerSymbol=&quot;STOCK0&quot; Date=&quot;2008-02-26&quot;&gt;75.00&lt;/MarketHistoryItem&gt;        /// [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string MarketHistory {
             get {
