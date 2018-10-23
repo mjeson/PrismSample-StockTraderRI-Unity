2632da9c45397955f75a6779fdd92b3636fe9433
GHislain1 <Gzeleu@gmx.de>
2018-10-23 18:21:15 +0200

    Refernce ASP.NEt to  Frameweo

--- StockTraderRI.Modules.Home/Program.cs
+++ StockTraderRI.Modules.Home/Program.cs
@@ -1,24 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
 namespace StockTraderRI.Modules.Home
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using Microsoft.AspNetCore;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.Logging;
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+  public class Program
+  {
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+
+    public static void Main(string[] args)
+    {
+      CreateWebHostBuilder(args).Build().Run();
     }
+  }
 }
